86a2b826dd9b467e0de0e0e09bbf215e25df6368
Roosevelt <panta_85.x@hotmail.com>
2020-05-24 21:35:35 -0500

    Updates

--- precache-manifest.888cdba90e89f9489a03e11afb33d5db.js
+++ precache-manifest.888cdba90e89f9489a03e11afb33d5db.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1bc93c7b0049e9f9de43d40ad1702b29",
+    "revision": "a1e192bbbd2712b48e8193a622d8634e",
     "url": "/barcode/index.html"
   },
   {
-    "revision": "9c1afc889733442f9c12",
-    "url": "/barcode/static/css/main.5c104597.chunk.css"
+    "revision": "ad0415f10142f8d1301c",
+    "url": "/barcode/static/css/main.b2859d19.chunk.css"
   },
   {
     "revision": "8e9136d379f9c5f0cf6e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/barcode/static/js/2.959090d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c1afc889733442f9c12",
-    "url": "/barcode/static/js/main.646b845a.chunk.js"
+    "revision": "ad0415f10142f8d1301c",
+    "url": "/barcode/static/js/main.4793f118.chunk.js"
   },
   {
     "revision": "0e18b34da6cd39baf60b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/barcode/precache-manifest.f443a1a36dc5c1bdac63bd676f370429.js"
+  "/barcode/precache-manifest.888cdba90e89f9489a03e11afb33d5db.js"
 );
 
 self.addEventListener('message', (event) => {
